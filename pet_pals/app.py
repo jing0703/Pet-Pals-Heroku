94c4c4611864ecf80aa11892079083ab4431f570
Jing X <vera.jxu@gmail.com>
2018-09-24 23:41:25 -0400

    make it better

--- pet_pals/app.py
+++ pet_pals/app.py
@@ -21,8 +21,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "s
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
 db = SQLAlchemy(app)
 
-from models import Pet
+class Pet(db.Model):
+    __tablename__ = 'pets'
 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    lat = db.Column(db.Float)
+    lon = db.Column(db.Float)
+
+    def __init__(self,name,lat,lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+
+    def __repr__(self):
+        return '<Pet %r>' % (self.name)
 
 # create route that renders index.html template
 @app.route("/")
